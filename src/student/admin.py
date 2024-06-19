e8bb6a575398c5d6a46b68c01963483ec5749ce5
= <=>
2024-06-19 21:09:16 +0530

    update

--- src/student/admin.py
+++ src/student/admin.py
@@ -1,9 +1,8 @@
 from django.contrib import admin
-from .models import CurrentStudentProfile, DroppedOutStudentProfile, PassedOutStudentProfile
-
+from .models import StudentProfile
 # Register your models here.
 
-
+@admin.register(StudentProfile)
 class StudentProfileAdmin(admin.ModelAdmin):
     search_fields = ['user__full_name', 'registration_number',
                      'course', 'registration_year']
@@ -20,48 +19,3 @@ class StudentProfileAdmin(admin.ModelAdmin):
     @admin.display(ordering='user__full_name', description='Name')
     def user__full_name(self, obj):
         return obj.user.full_name
-
-
-@admin.register(CurrentStudentProfile)
-class CurrentStudentProfileAdmin(StudentProfileAdmin):
-    list_display = ['user__full_name', 'registration_number',
-                    'course', 'registration_year']
-    list_filter = ['course']
-    fieldsets = [
-        (
-            None,
-            {
-                'fields': ['user', ('id_number'), 'course', ('registration_number', 'registration_year')],
-            }
-        ),
-    ]
-
-
-@admin.register(PassedOutStudentProfile)
-class CurrentStudentProfileAdmin(StudentProfileAdmin):
-    list_display = ['user__full_name', 'registration_number',
-                    'course', 'registration_year']
-    list_filter = ['course']
-    fieldsets = [
-        (
-            None,
-            {
-                'fields': ['user', ('id_number'), 'course', ('registration_number', 'registration_year'), 'pass_out_year'],
-            }
-        ),
-    ]
-
-
-@admin.register(DroppedOutStudentProfile)
-class CurrentStudentProfileAdmin(StudentProfileAdmin):
-    list_display = ['user__full_name', 'registration_number',
-                    'course', 'registration_year']
-    list_filter = ['course']
-    fieldsets = [
-        (
-            None,
-            {
-                'fields': ['user', ('id_number'), 'course', ('registration_number', 'registration_year')],
-            }
-        ),
-    ]
