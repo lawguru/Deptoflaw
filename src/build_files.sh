d37d17a023b9d9304012c7c4fdf61d13bc2a334f
= <=>
2024-06-20 10:38:39 +0530

    update

--- src/build_files.sh
+++ src/build_files.sh
@@ -1,4 +1,4 @@
-pip install -r requirements.txt
-python manage.py makemigrations
-python manage.py migrate
-python manage.py runserver
+python3.10 -m pip install -r requirements.txt
+python3.10 manage.py makemigrations
+python3.10 manage.py migrate
+python3.10 manage.py runserver
