a56d28684a16ce4d69f1a072fa381f46e3888828
= <=>
2024-06-20 12:23:47 +0530

    update

--- src/main/settings.py
+++ src/main/settings.py
@@ -83,22 +83,10 @@ WSGI_APPLICATION = 'main.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': BASE_DIR / 'db.sqlite3',
-#     }
-# }
-
-
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'railway',
-        'USER': 'postgres',
-        'PASSWORD': 'xMszsWxDXaPEhOgylPeztVIsjMxiAidK',
-        'HOST': 'monorail.proxy.rlwy.net',
-        'PORT': '21925',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
 
