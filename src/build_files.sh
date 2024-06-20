6e6392cc3515be9284a066ee81f4d90371b9ad15
= <=>
2024-06-20 12:36:20 +0530

    migrations and previous database

--- src/build_files.sh
+++ src/build_files.sh
@@ -3,3 +3,6 @@ source venv/bin/activate
 
 pip install -r requirements.txt 
 python3.12 manage.py collectstatic
+python3.12 manage.py makemigrations
+python3.12 manage.py migrate
+python3.12 manage.py loaddata database.json
