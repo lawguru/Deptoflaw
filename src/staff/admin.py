629ae1a34c02139f340a5761269df2431ea5ac2e
= <=>
2024-06-20 12:01:10 +0530

    update

--- src/staff/admin.py
+++ src/staff/admin.py
@@ -1,21 +1,3 @@
 from django.contrib import admin
-from .models import StaffProfile
 
 # Register your models here.
-@admin.register(StaffProfile)
-class StaffProfileAdmin(admin.ModelAdmin):
-  list_display = ("user__full_name", "id_number", "designation", "qualification")
-  list_filter = ('designation', 'qualification')
-
-  fieldsets = [
-    (
-      None,
-      {
-        'fields': ['user', 'id_number', ('designation', 'qualification'), ('is_hod', 'is_tpc_head')],
-      }
-    ),
-  ]
-
-  @admin.display(ordering='user__full_name', description='Name')
-  def user__full_name(self, obj):
-    return obj.user.get_full_name()
--- src/student/admin.py
+++ src/student/admin.py
@@ -1,21 +1,3 @@
 from django.contrib import admin
-from .models import StudentProfile
+
 # Register your models here.
-
-@admin.register(StudentProfile)
-class StudentProfileAdmin(admin.ModelAdmin):
-    search_fields = ['user__full_name', 'registration_number',
-                     'course', 'registration_year']
-    readonly_fields = ['registration_year', 'pass_out_year']
-    fieldsets = [
-        (
-            None,
-            {
-                'fields': ['user', ('id_card', 'id_number'), 'course', ('registration_number', 'registration_year'), ('passed_out', 'is_current')],
-            }
-        ),
-    ]
-
-    @admin.display(ordering='user__full_name', description='Name')
-    def user__full_name(self, obj):
-        return obj.user.full_name
