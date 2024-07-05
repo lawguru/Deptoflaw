7b46ffef9e131a4dc2077fdbd56c0a76d3b23b0a
= <=>
2024-07-05 11:05:06 +0530

    Directory Structure and npm dependency management

--- src/build.sh
+++ src/build.sh
@@ -1,7 +1,6 @@
-python3.12 -m venv venv
-source venv/bin/activate
-
-pip install -r requirements.txt 
-python3.12 manage.py collectstatic
-# python3.12 manage.py makemigrations
-# python3.12 manage.py migrate
+npm install
+npm run venv
+npm run activate
+npm run pip-install
+npm run collectstatic
+npm run scss
