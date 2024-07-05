f3915554ec4a78519c6f2be0be2eba9dbdf58360
= <=>
2024-07-05 11:23:15 +0530

    fix vercel build.sh

--- src/build.sh
+++ src/build.sh
@@ -1,8 +1,9 @@
+npm install
+npm run scss
+
 python3.12 -m venv venv
 source venv/bin/activate
 
-pip install -r ./project/requirements.txt
-python3.12 ./project/manage.py collectstatic
-
-npm install
-npm run scss
+cd project
+pip install -r requirements.txt
+python3.12 manage.py collectstatic
